f9e804524c8d0a396e5b84a561b91f963465f7f1
Arturo Filio Villa <arturfil@hotmail.com>
2019-07-12 19:49:24 -0400

    created the folder structure separations and started adding the navigation and the view files

---
+++ app/src/main/java/com/arturofilio/mvvm_users/view/ListFragment.kt
@@ -0,0 +1,22 @@
+package com.arturofilio.mvvm_users.view
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.arturofilio.mvvm_users.R
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+
+}
